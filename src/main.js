122c95bf696d1a19ca72ef376038283555bd84d4
Melvin Idema <melvin_i13@hotmail.com>
2021-11-03 16:33:20 +0100

    Found out I did not need the numToPositive function. You can just do currentYear - yearInThePast instead of yearInThePast - currentYear. 🤦🏼‍♂️

--- src/main.js
+++ src/main.js
@@ -1,30 +1,19 @@
-import api from "./config/api.js";
+import api from "./helpers/api.js";
 
-import numToPositive from './helpers/numToPositive.js';
 import grabYear from './helpers/grabYear.js';
 import describeThePh from "./helpers/describePh.js";
-
-const $ = selector => document.querySelector(selector);
 
 /**
  * Not a functional Function. Has side effects:
  * - Uses outside functions
  * This will turn a given year (in the past) into a "x years ago" string.
- * @param year
- * @returns {string}
  */
-const yearToText = year => `First brewed ${numToPositive((parseInt(grabYear(year))) - new Date().getFullYear())} years ago`;
+const yearToText = year => `First brewed ${new Date().getFullYear() - parseInt(grabYear(year))} years ago`;
 
 /**
- *
  * Transforms certain properties in the beer object.
  *
  * Uses dependency injection to avoid any side effects.
- *
- * @param describePh
- * @param transformFirstBrewed
- * @param dirtyBeer
- * @returns {*&{ph: string, firstBrewed: string}}
  */
 const transformBeer = ({describePh, transformFirstBrewed}, dirtyBeer) => ({
     ...dirtyBeer,
@@ -32,12 +21,16 @@ const transformBeer = ({describePh, transformFirstBrewed}, dirtyBeer) => ({
     firstBrewed: transformFirstBrewed(dirtyBeer.firstBrewed),
 });
 
+/**
+ * Inserts the data from the beer object in the
+ * proper HTML elements.
+ */
 function renderBeer(beer) {
-    const thumb = $("#beerThumb");
-    const title = $("#beerTitle");
-    const desc = $("#beerDesc");
-    const sour = $("#beerSour");
-    const brewed = $("#beerBrewed");
+    const thumb = document.querySelector("#beerThumb");
+    const title = document.querySelector("#beerTitle");
+    const desc = document.querySelector("#beerDesc");
+    const sour = document.querySelector("#beerSour");
+    const brewed = document.querySelector("#beerBrewed");
 
     thumb.src = beer.image;
     title.innerText = beer.name;
@@ -52,9 +45,9 @@ function renderBeer(beer) {
  * - Makes changes to DOM Element
  * - Accesses API Object and outside functions.
  */
-function giveMeARandomBeer({describePh, transformFirstBrewed}, dirtyBeer) {
-    const startHeader = $(".start-header");
-    const beerSection = $(".beer-section");
+function giveMeARandomBeer() {
+    const startHeader = document.querySelector(".start-header");
+    const beerSection = document.querySelector(".beer-section");
     if (startHeader.dataset.started === "false") {
         startHeader.dataset.started = "true";
         startHeader.classList.add("to-the-left");
